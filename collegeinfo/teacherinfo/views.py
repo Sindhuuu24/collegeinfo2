584edacd29888db14f3fbe2b9465f2ad3f464c1b
Ajmeerakalyani19 <23641a6604@vaagdevi.edu.in>
2025-12-06 13:18:47 -0600

    dashboard.html

--- collegeinfo/teacherinfo/views.py
+++ collegeinfo/teacherinfo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 def landing(request):
     return render(request, 'landing.html')
+def dashboard(request):
+    return render(request,'dashboard.html')
 
 # Create your views here.
