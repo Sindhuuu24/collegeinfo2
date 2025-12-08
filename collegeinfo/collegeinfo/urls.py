fbb37a054bd95f72bb822f72d6cc9373a903a023
srivalli <kolasrivalli3@gmail.com>
2025-12-08 10:53:21 +0530

    saves

--- collegeinfo/collegeinfo/urls.py
+++ collegeinfo/collegeinfo/urls.py
@@ -17,13 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from teacherinfo.views import landing
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from teacherinfo.views import login, register,registerinfo
-=======
-<<<<<<< HEAD
-=======
->>>>>>> bc919b2bb79a9a7cab3cac3602e71422b2aa5041
+
 from teacherinfo. views  import dashboard
 from teacherinfo.views import registration
 from teacherinfo.views import dashboard
@@ -32,10 +28,6 @@ from teacherinfo.views import registerinfo
 from teacherinfo.views import login
 from teacherinfo.views import home
 from teacherinfo.views import department,settings,faculty
-
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/', landing, name='landing'),
@@ -44,30 +36,7 @@ urlpatterns = [
     path('registerinfo/', registerinfo, name='registerinfo'),
     path('registration/', registration, name='registration'),
     path('dashboard/',dashboard,name='dashboard'),
-<<<<<<< HEAD
->>>>>>> dashboard
-=======
-from teacherinfo.views import login
->>>>>>> 2014a9498c4241ae716d6c176e77ae9738d34f37
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('landing/', landing, name='landing'),
-    path('login/', login, name='login'),
-<<<<<<< HEAD
-    path('register/', register, name='register'),
-    path('registerinfo/', registerinfo, name='registerinfo'),
-=======
->>>>>>> login
->>>>>>> 349b643d65a5e6f0ab2f65bd4902ca701b7a9315
->>>>>>> 2014a9498c4241ae716d6c176e77ae9738d34f37
-=======
-    path('admin/', admin.site.urls),
-    path('landing/', landing, name='landing'),
-    path('login/', login, name='login'),
-    path('home/', home, name='home'),
-    path('department/', department, name='department'),
-    path('settings/', settings, name='settings'),
-    path('faculty/', faculty, name='faculty'),
-
->>>>>>> bc919b2bb79a9a7cab3cac3602e71422b2aa5041
+    path('faculty/',faculty,name='faculty'),
+    path('settings/',settings,name='settings'),
 ]
+
--- collegeinfo/teacherinfo/admin.py
+++ collegeinfo/teacherinfo/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import TeacherInfocd
-admin.site.register(TeacherInfo)
-=======
 from .models import Teacherinfo
 admin.site.register(Teacherinfo)
 
->>>>>>> 2014a9498c4241ae716d6c176e77ae9738d34f37
 # Register your models here.
